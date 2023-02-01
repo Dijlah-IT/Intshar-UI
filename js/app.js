99828747ffb6655ce6f2fc1014b6951fe54e8baa
zoheir kabuli <zoheyrk@gmail.com>
2023-02-01 17:36:27 +0330

    remove log

--- js/app.js
+++ js/app.js
@@ -119,8 +119,6 @@ const darkModeToggle = document.getElementById("darkmode-toggle");
 const userTheme = localStorage.getItem("theme");
 const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-console.log(userTheme, systemTheme);
-
 const themeCheck = () => {
   if (userTheme === "dark" || (!userTheme && systemTheme)) {
     document.documentElement.classList.add("dark");
